0be61c61fd668c46b7d989686b6705b17784be93
MatthewVale <matt.vale1@gmail.com>
2021-12-04 17:47:38 +0000

    Added contact form

--- matthewvale/lets-talk.php
+++ matthewvale/lets-talk.php
@@ -8,5 +8,17 @@
         <div class="box-promo">
             <img src="res\images\promos\anwk_promo.jpg" alt="A New World: Kingdoms">
         </div>
+        <h1>LET'S TALK</h1>
+        <div class="contact-form-container">
+            <form action="mailto:matt.vale1@gmail.com" method="POST">
+                <label for="fname">Your Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your name...">
+                <label for="subject">Your Subject</label>
+                <input type="text" id="subject" name="subject" placeholder="Subject...">
+                <label for="subject">Your message...</label>
+                <textarea id="subject" name="subject" placeholder="Write your message here..." style="height:200px"></textarea>
+                <input type="submit" value="SUBMIT">
+            </form>
+        </div>
     </body>
 </html>
